c1b08372307011dc396052b1d736943da5c42f3b
Chase Rivera <achaserivera@gmail.com>
2021-05-20 18:44:19 -0700

    declared uppercase variable and added array of upper case letters

--- Develop/script.js
+++ Develop/script.js
@@ -2,6 +2,7 @@
 var generateBtn = document.querySelector("#generate");
 
 var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
 // Write password to the #password input
 function writePassword() {
