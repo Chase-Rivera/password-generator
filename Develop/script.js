79cc8f0381faba2c05ac578781dd5bbef93ddc42
Chase Rivera <achaserivera@gmail.com>
2021-05-20 18:40:40 -0700

    declared lowercase as variable and added an array of lower case letters

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
